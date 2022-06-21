0788510c404fdf631e433b5ea866e85dd2f91006
may-ha <101331038+may-ha@users.noreply.github.com>
2022-06-21 19:13:17 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+/*jQuery(document).ready(function ($) {
+  // This is for the auto sliding
+  setInterval(function () {
+    moveRight();
+  }, 3000);
+
+  //variables
+  var slideCount = $("#slider ul li").length;
+  var slideWidth = $("#slider ul li").width();
+  var slideHeight = $("#slider ul li").height();
+  var sliderUlWidth = slideCount * slideWidth;
+
+  $("#slider").css({ width: slideWidth, height: slideHeight });
+
+  $("#slider ul").css({ width: sliderUlWidth, marginLeft: -slideWidth });
+
+  $("#slider ul li:last-child").prependTo("#slider ul");
+
+  function moveLeft() {
+    $("#slider ul").animate(
+      {
+        left: +slideWidth,
+      },
+      300,
+      function () {
+        $("#slider ul li:last-child").prependTo("#slider ul");
+        $("#slider ul").css("left", "");
+      }
+    );
+  }
+
+  function moveRight() {
+    $("#slider ul").animate(
+      {
+        left: -slideWidth,
+      },
+      300,
+      function () {
+        $("#slider ul li:first-child").appendTo("#slider ul");
+        $("#slider ul").css("left", "");
+      }
+    );
+  }
+
+  $(".control_prev").click(function () {
+    moveLeft();
+  });
+
+  $(".control_next").click(function () {
+    moveRight();
+  });
+});
+*/
+var slides = document.querySelectorAll(".slide");
+var dots = document.querySelectorAll(".dot");
+var index = 0;
+
+function prevSlide(n) {
+  index += n;
+  console.log("prevSlide is called");
+  changeSlide();
+}
+
+function nextSlide(n) {
+  index += n;
+  changeSlide();
+}
+
+changeSlide();
+
+function changeSlide() {
+  if (index > slides.length - 1) index = 0;
+
+  if (index < 0) index = slides.length - 1;
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+
+    dots[i].classList.remove("active");
+  }
+
+  slides[index].style.display = "block";
+  dots[index].classList.add("active");
+}
